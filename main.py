36aa4ed9a1451183facb579e42b9ebcf54923731
vidishraj <vidishraj@gmail.com>
2025-01-17 00:05:20 +0530

    main init

---
+++ main.py
@@ -0,0 +1,12 @@
+from graph import Graph
+
+if __name__ == '__main__':
+
+    edges = [
+        (1, 2), (2, 3), (3, 4), (4, 5), (5, 1),
+        (1, 6), (6, 7), (7, 8), (8, 9), (9, 1),
+        (3, 7), (5, 9), (6, 4), (2, 8)
+    ]
+    gh = Graph(edges)
+    al = gh.buildAdjacencyList()
+    gh.printAdjacencyList(al)
